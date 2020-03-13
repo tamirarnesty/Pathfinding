8b96232c90aff5a3d43861a8ab2458530dac4f17
danielGoldsteinCS2021 <18djg@queensu.ca>
2020-03-12 23:40:13 -0400

    added a bit of code

--- pathfinding.py
+++ pathfinding.py
@@ -1,5 +1,6 @@
 import sys
-
+from math import sqrt
+import heapq
 
 class Pathfinding:
 
@@ -7,53 +8,41 @@ class Pathfinding:
         self.extension = ".txt"
         if fileName.endswith(self.extension):
             fileName = fileName[:-len(self.extension)]
-
         self.fileName = fileName
         self.canDoDiagonal = canDiagonal
-
         self.grid = []
         self.startPosition = None
         self.goalPosition = None
+        self.currentPosition = None
         self.numRows = 0
         self.numColumns = 0
-
         self.readFile()
 
     def readFile(self):
         file = open(self.fileName + self.extension, 'r')
-
         originalOutput = sys.stdout
         outFileName = self.fileName + "_out" + self.extension
         sys.stdout = open(outFileName, 'w')
-
-        m = 0
-        n = 0
-
+        m, n = 0, 0
         startPos = None
         goalPos = None
-
         lines = file.readlines()
         for i in range(len(lines)):
             cleaned = lines[i].strip()
-
             if cleaned != "":
                 row = list(cleaned)
-
                 try:
                     indexOfS = row.index("S")
                 except ValueError:
                     indexOfS = None
-
                 try:
                     indexOfG = row.index("G")
                 except ValueError:
                     indexOfG = None
-
                 if indexOfS:
                     startPos = (n, indexOfS)
                 if indexOfG:
                     goalPos = (n, indexOfG)
-
                 m = len(row)
                 self.grid.append(list(cleaned))
                 n += 1
@@ -62,7 +51,6 @@ class Pathfinding:
                 n = 0
                 self.grid = []
                 file.readline()
-
         # Last grid to run
         self.runAlgorithms(m, n, startPos, goalPos)
 
@@ -73,23 +61,37 @@ class Pathfinding:
         self.numColumns = n
         self.startPosition = startPos
         self.goalPosition = goalPos
-
+        self.currentPosition = startPos
         self.AStar()
         self.greedy()
-
         print()
+
+    def manhattenDistance(self, curPos):
+        row, col = curPos
+        rowDifference = abs(row - self.goal[0])
+        colDifference = abs(col - self.goal[1])
+        return rowDifference+colDifference
+
+    def euclidenDistance(self, curPos):
+        row, col = curPos
+        rowDifference = abs(row - self.goal[0])
+        colDifference = abs(col - self.goal[1])
+        cSquared = rowDifference*rowDifference + colDifference*colDifference
+        c = sqrt(cSquared)
+        return c
 
     def AStar(self):
         self.printGrid("A*")
-        pass
-
+        frontier = []
+        frontier = heapq.heapify(frontier)
+        came_from = {} # use a dictionary to generate path by linkage
+        # do the rest later --
     def greedy(self):
         self.printGrid("Greedy")
         pass
 
     def printGrid(self, algorithm):
         print(algorithm)
-
         for row in self.grid:
             for item in row:
                 print(item, end="")
